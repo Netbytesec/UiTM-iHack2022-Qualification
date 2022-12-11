dfd3f8451768666d71ff9a3ab38f80c82ead1827
hazmiae <hazmiae@gmail.com>
2022-12-11 18:36:38 +0800

    web4

---
+++ Web/web4/about.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Catsp">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>About</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="About.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.9.3, nicepage.com">
+    
+    
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    
+    
+  
+    <meta property="og:title" content="About">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.php">
+    <meta property="og:url" content="index.php">
+  </head>
+  <body class="u-body"><header class="u-clearfix u-grey-70 u-header u-header" id="sec-252d"><div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-align-left u-text u-title u-text-1">Laptop Store</span>
+        </h1>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px; font-weight: 700;">
+            <a class="u-button-style u-custom-active-border-color u-custom-active-color u-custom-border u-custom-border-color u-custom-borders u-custom-color u-custom-hover-border-color u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg><use xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-spacing-20 u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-active-grey-5 u-border-active-palette-1-base u-border-hover-palette-1-base u-border-no-left u-border-no-right u-border-no-top u-button-style u-grey-70 u-nav-link u-text-active-palette-2-base u-text-hover-white" href="/page.php?show=index.php" style="padding: 10px;">Home</a>
+</li><li class="u-nav-item"><a class="u-active-grey-5 u-border-active-palette-1-base u-border-hover-palette-1-base u-border-no-left u-border-no-right u-border-no-top u-button-style u-grey-70 u-nav-link u-text-active-palette-2-base u-text-hover-white" href="/page.php?show=about.php" style="padding: 10px;">About</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/page.php?show=index.php" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/page.php?show=about.php" style="padding: 10px 20px;">About</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section id="carousel_200b" class="u-carousel u-slide u-block-51d0-1" data-u-ride="carousel" data-interval="5000">
+      <ol class="u-absolute-hcenter u-carousel-indicators u-block-51d0-2">
+        <li data-u-target="#carousel_200b" data-u-slide-to="0" class="u-active u-grey-30"></li>
+        <li data-u-target="#carousel_200b" data-u-slide-to="1" class="u-grey-30"></li>
+        <li data-u-target="#carousel_200b" class="u-grey-30" data-u-slide-to="2"></li>
+      </ol>
+      <div class="u-carousel-inner" role="listbox">
+        <div class="u-active u-align-center u-carousel-item u-clearfix u-image u-shading u-section-1-1" src="" data-image-width="1534" data-image-height="1000">
+          <div class="u-clearfix u-sheet u-sheet-1">
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="u-border-2 u-border-white u-btn u-btn-round u-button-style u-hover-custom-color-1 u-radius-50 u-btn-1">Flag</a>
+          </div>
+        </div>
+        <div class="u-align-center u-carousel-item u-clearfix u-image u-shading u-section-1-2" src="" data-image-width="1280" data-image-height="850">
+          <div class="u-clearfix u-sheet u-sheet-1">
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="u-border-1 u-border-white u-btn u-btn-round u-button-style u-hover-palette-1-dark-1 u-radius-48 u-radius-50 u-btn-1">Facebook</a>
+          </div>
+        </div>
+      </div>
+      <a class="u-absolute-vcenter u-carousel-control u-carousel-control-prev u-text-grey-30 u-block-51d0-3" href="#carousel_200b" role="button" data-u-slide="prev">
+        <span aria-hidden="true">
+          <svg viewBox="0 0 477.175 477.175"><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225
+                    c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"></path></svg>
+        </span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="u-absolute-vcenter u-carousel-control u-carousel-control-next u-text-grey-30 u-block-51d0-4" href="#carousel_200b" role="button" data-u-slide="next">
+        <span aria-hidden="true">
+          <svg viewBox="0 0 477.175 477.175"><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5
+                    c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"></path></svg>
+        </span>
+        <span class="sr-only">Next</span>
+      </a>
+    </section>
+    <section class="u-align-center u-clearfix u-section-2" src="" id="carousel_9607">
+      <h1 class="u-custom-font u-font-montserrat u-text u-text-1">Key Features</h1>
+      <img class="u-expanded-width u-image u-image-1" src="images/laptop2.jpg" data-image-width="1280" data-image-height="858">
+      <div class="u-list u-repeater u-list-1">
+        <div class="u-align-center u-container-style u-list-item u-palette-4-base u-repeater-item u-shape-rectangle u-video-cover u-list-item-1">
+          <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-1"><span class="u-icon u-icon-circle u-text-white u-icon-1">
+            <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 128 128" style=""><use xlink:href="#svg-5e7a"></use></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 128 128" id="svg-5e7a"><polygon points="104 40 104 48 120 48 120 120 48 120 48 104 40 104 40 128 128 128 128 40"></polygon><rect x="80" width="8" height="8"></rect><rect x="16" width="8" height="8"></rect><rect x="32" width="8" height="8"></rect><rect x="16" y="80" width="8" height="8"></rect><rect x="48" width="8" height="8"></rect><rect x="64" width="8" height="8"></rect><rect width="8" height="8"></rect><rect y="48" width="8" height="8"></rect><rect y="64" width="8" height="8"></rect><rect y="32" width="8" height="8"></rect><rect y="16" width="8" height="8"></rect><rect x="80" y="16" width="8" height="8"></rect><rect y="80" width="8" height="8"></rect><path d="m63.6 96.8 10.4 10.4 36.8-36.8-10.6-10.6-5.7-17-19.6-19.5-39.5 16.2c-6.1 2.5-9.1 9.5-6.5 15.7l0.3 0.6-0.5 0.5c-6.2 6.2-6.2 16.4 0 22.6l15.1 15.1 19.8 2.8zm-31.6-29.2c0-2.1 0.8-4.2 2.3-5.7l14.1 14.1 5.7-5.7-17-17c-1.6-1.6-1.6-4.1 0-5.7 0.4-0.4 0.8-0.7 1.3-0.9l34.6-14 14.4 14.4 5.7 17 6.3 6.3-25.5 25.5-6.6-6.6-19.8-2.8-13.2-13.3c-1.5-1.5-2.3-3.5-2.3-5.6z"></path></svg>
+          </span>
+            <h4 class="u-align-center u-text u-text-body-alt-color u-text-2">Projects</h4>
+            <p class="u-align-center u-custom-font u-font-georgia u-text u-text-body-alt-color u-text-3" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">100+</p>
+          </div>
+        </div>
+        <div class="u-align-center u-container-style u-custom-background u-list-item u-palette-2-base u-repeater-item u-shape-rectangle u-video-cover u-list-item-2">
+          <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-2"><span class="u-icon u-icon-circle u-text-white u-icon-2">
+            <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 128 128" style=""><use xlink:href="#svg-e6b7"></use></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 128 128" id="svg-e6b7"><path d="m102.7 39.2c0.8 0 1.5 0.3 2.1 0.9l7.8 7.8c4.8 4.8 7.7 11.5 7.4 18.3-0.6 12.6-11 22.6-23.7 22.6h-29c-0.1 0-0.1-0.1-0.1-0.2 1.1-3 4.1-5.2 7.5-5.2h10.5c0.9 0 1.8-0.4 2.3-1.1 0.8-1.1 0.7-2.6-0.3-3.5l-24.9-24.8c-0.6-0.6-0.9-1.3-0.9-2.1s0.3-1.5 0.9-2.1 1.3-0.8 2.1-0.8 1.5 0.3 2.1 0.8l16.9 16.9c0.5 0.5 1.2 0.8 1.9 0.8s1.4-0.3 1.9-0.8 0.8-1.2 0.8-1.9-0.3-1.4-0.8-1.9l-6.7-6.7c-0.6-0.6-0.9-1.3-0.9-2.1s0.3-1.5 0.9-2.1 1.3-0.8 2.1-0.8 1.5 0.3 2.1 0.8l4 4c0.5 0.5 1.2 0.8 1.9 0.8s1.4-0.3 1.9-0.8 0.8-1.2 0.8-1.9-0.3-1.4-0.8-1.9l-2.7-2.7c-0.5-0.5-0.8-1.2-0.8-2s0.3-1.5 0.9-2.1 1.3-0.9 2.1-0.9 1.5 0.3 2.1 0.9l2.7 2.7c0.5 0.5 1.2 0.8 1.9 0.8s1.4-0.3 1.9-0.8 0.8-1.2 0.8-1.9-0.2-1.3-0.7-1.8c-0.8-0.9-1.2-2.1-0.8-3.3 0.5-1.3 1.6-1.9 2.8-1.9m0-8c-2.9 0-5.7 1.1-7.8 3.2-0.7 0.7-1.2 1.4-1.7 2.1 0 0-0.1 0.1-0.1 0-0.4 0-0.8-0.1-1.1-0.1-2.9 0-5.7 1.1-7.8 3.2-1 1-1.8 2.2-2.3 3.4 0 0 0 0.1-0.1 0.1-2.6 0.2-5.1 1.3-6.9 3.2l-0.2 0.2-2.5-2.5c-2-2-4.9-3.2-7.8-3.2s-5.7 1.1-7.8 3.2-3.2 4.9-3.2 7.8c0 3 1.2 5.7 3.2 7.8l15.8 15.8c0.1 0.1 0 0.2-0.1 0.2-5.8 0.8-10.8 4.8-12.8 10.5-1.9 5.2 2 10.7 7.5 10.7h29.1c8.5 0 16.4-3.3 22.4-9.3 6.1-6.1 9.4-14.3 9.3-23-0.1-8.4-3.8-16.4-9.8-22.4l-7.8-7.8c-1.8-2-4.6-3.1-7.5-3.1z"></path><path d="m82.1 118c0 1.1-0.9 2-2 2h-70.2c-1.1 0-2-0.9-2-2v-108c0-1.1 0.9-2 2-2h70.1c1.1 0 2 0.9 2 2v16.9c0 2.2 1.8 4 4 4s4-1.8 4-4v-16.9c0-5.5-4.5-10-9.9-10h-70.2c-5.4 0-9.9 4.5-9.9 10v108c0 5.5 4.5 10 9.9 10h70.1c5.5 0 9.9-4.5 9.9-10v-10.6c0-2.2-1.8-4-4-4s-4 1.8-4 4v10.6z"></path></svg>
+          </span>
+            <h4 class="u-align-center u-text u-text-body-alt-color u-text-4">Subscribers</h4>
+            <p class="u-align-center u-custom-font u-font-georgia u-text u-text-body-alt-color u-text-5" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">400+</p>
+          </div>
+        </div>
+        <div class="u-align-center u-container-style u-custom-background u-list-item u-palette-4-base u-repeater-item u-shape-rectangle u-video-cover u-list-item-3">
+          <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-3"><span class="u-icon u-icon-circle u-text-white u-icon-3">
+            <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 128 128" style=""><use xlink:href="#svg-4ebb"></use></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 128 128" id="svg-4ebb"><polygon points="128 3 17 3 17 24 25 24 25 11 120 11 120 24 111 24 111 32 120 32 120 95 111 95 111 103 128 103"></polygon><path d="m25 24h-25v100h111v-100h-86zm78 92h-95v-63h95v63zm0-71h-95v-13h95v13z"></path><path d="m46 74v21h-21v-21h21m8-8h-37v37h37v-37z"></path><rect x="62" y="66" width="32" height="8"></rect><rect x="62" y="81" width="32" height="8"></rect><rect x="62" y="95" width="32" height="8"></rect></svg>
+          </span>
+            <h4 class="u-align-center u-text u-text-body-alt-color u-text-6">Views</h4>
+            <p class="u-align-center u-custom-font u-font-georgia u-text u-text-body-alt-color u-text-7" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">5000+</p>
+          </div>
+        </div>
+        <div class="u-align-center u-container-style u-custom-background u-list-item u-palette-2-base u-repeater-item u-shape-rectangle u-video-cover u-list-item-4">
+          <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-4"><span class="u-icon u-icon-circle u-text-white u-icon-4">
+            <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 128 128" style=""><use xlink:href="#svg-d43c"></use></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 128 128" id="svg-d43c"><path d="m40.6 11c2.3 0 4.4 0.9 6.1 2.5 5.4 5.4 10.8 19.2 8.9 21.2 0 0-0.3 0.3-1.5 0.3-5.5 0-16.1-5.7-19.5-9.2-3.4-3.4-3.4-8.9 0-12.2 1.5-1.7 3.7-2.6 6-2.6m46.8 0.1c2.2 0 4.4 0.8 6.1 2.4 3.4 3.4 3.4 8.9 0 12.2-3.4 3.5-14 9.2-19.5 9.2-1.1 0-1.5-0.3-1.5-0.3-1.9-1.9 3.5-15.7 8.9-21.2 1.6-1.5 3.8-2.3 6-2.3m32.6 32.1v16h-52v-16h52m-60 0v16h-52v-16h52m52 23.9v47.9h-44v-47.9h44m-52 0v47.9h-44v-47.9h44m-19.4-64.1c-4.4 0-8.6 1.7-11.7 4.9-6.5 6.5-6.5 17 0 23.5 1.2 1.2 2.8 2.5 4.7 3.8h-25.6c-4.4 0-8 3.6-8 8v24h8v55.8h112v-55.9h8v-24c0-4.4-3.6-8-8-8h-25.6c1.9-1.3 3.6-2.6 4.7-3.8 6.5-6.5 6.5-17 0-23.5-3-3-7.3-4.8-11.7-4.8s-8.7 1.7-11.7 4.8c-4.6 4.6-7.8 11.7-8.9 14.5-2.2 5.4-3 9.5-2.5 12.8h-0.5c0.5-3.2-0.4-7.4-2.5-12.8-1.2-2.8-4.3-9.9-8.9-14.5-3.2-3.1-7.4-4.8-11.8-4.8z"></path></svg>
+          </span>
+            <h4 class="u-align-center u-text u-text-body-alt-color u-text-8">Likes</h4>
+            <p class="u-align-center u-custom-font u-font-georgia u-text u-text-body-alt-color u-text-9" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">1000+</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-64e7"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Yes, This is Footer!!!</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>
